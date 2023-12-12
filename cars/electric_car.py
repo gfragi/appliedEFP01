d0293832ad9ade575fdbb0736bd84987ef7274ee
Giorgos Fragiadakis <gfragi@hua.gr>
2023-12-12 17:58:50 +0200

    car classes

---
+++ cars/electric_car.py
@@ -0,0 +1,32 @@
+from car import Car
+
+class Battery:
+    """ A simple attempt to model a battery for an electric car. """
+
+    def __init__(self, battery_size=75):
+        """ Initialize the battery's attributes. """
+        self.battery_size = battery_size
+
+    def describe_battery(self):
+        """ Print a statement describing the battery size. """
+        print(f"This car has a {self.battery_size}-kWh battery.")
+
+    def get_range(self):
+        """ Print a statement about the range this battery provides. """
+        if self.battery_size == 75:
+            range = 260
+        elif self.battery_size == 100:
+            range = 315
+
+        print(f"This car can go about {range} miles on a full charge.")
+
+    def upgrade_battery(self):
+        """ Upgrade the battery if possible. """
+        if self.battery_size < 100:
+            self.battery_size = 100
+
+class ElectricCar(Car):
+    def __init__(self, make, model, year):
+        """ Initialize attributes of the parent class. """
+        super().__init__(make, model, year)
+        self.battery = Battery()
