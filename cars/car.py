e2f05f76dc989fe9d522791fd73f0ecaf4c8da23
gfragi <gfragi@hua.gr>
2023-12-19 18:53:33 +0200

    add a space

--- cars/car.py
+++ cars/car.py
@@ -3,7 +3,7 @@
 class Car:
     def __init__(self, make, model, year):
         """ Initialize attributes to describe a car. """
-        self.make = make
+        self.make = make 
         self.model = model
         self.year = year
         self.odometer_reading = 0
